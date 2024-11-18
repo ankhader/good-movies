6e1b33de8effc355758580d6bc406e9177f00245
Abdelrahman Khader <abdel-rahman.khader@progressoft.com>
2024-11-18 21:14:11 +0300

    replace resttemplate with restclient

--- movie-catalog-service/src/main/java/com/abdelrahman/movie_catalog_service/config/WebConfig.java
+++ movie-catalog-service/src/main/java/com/abdelrahman/movie_catalog_service/config/WebConfig.java
@@ -3,14 +3,14 @@ package com.abdelrahman.movie_catalog_service.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class WebConfig {
 
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestClient.Builder restClient() {
+        return RestClient.builder();
     }
 }
--- movie-catalog-service/src/main/java/com/abdelrahman/movie_catalog_service/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/abdelrahman/movie_catalog_service/controller/MovieCatalogController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,20 +17,20 @@ import java.util.stream.Collectors;
 public class MovieCatalogController {
     public static final String ACCOUNT_CUSTOMER_DETAILS_REQUEST_MAPPING = "/api/v1/catalog";
 
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
-    public MovieCatalogController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public MovieCatalogController(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.build();
     }
 
 
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/api/v1/ratings/user/" + userId, UserRating.class);
+        UserRating userRating = restClient.get().uri("http://ratings-data-service/api/v1/ratings/user/" + userId).retrieve().body(UserRating.class);
 
         return userRating.getRatings().stream()
                 .map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://movie-info-service/api/v1/movies/" + rating.getMovieId(),Movie.class);
+                    Movie movie = restClient.get().uri("http://movie-info-service/api/v1/movies/" + rating.getMovieId()).retrieve().body(Movie.class);
                     return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
                 })
                 .collect(Collectors.toList());
